433c95810085ab5b088d46942cb284818bcc7d6c
Christian Alemán <chris.aleman2005@gmail.com>
2024-03-04 10:02:10 -0400

    "new stuff"

---
+++ LabsC#/ejemploVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ejemploVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
