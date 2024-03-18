8db22169935a183b2d8a93371769201704955ac9
Christian Alemán <chris.aleman2005@gmail.com>
2024-03-18 19:29:43 -0400

    C++ file Tested

--- Labs C++/Actividad_1/1-PromedioFinal.cpp
+++ Labs C++/Actividad_1/1-PromedioFinal.cpp
@@ -2,4 +2,25 @@
 using namespace std;
 
 int main()
-{}
+{
+    //valores de los examenes
+    double examen1, examen2, examen3, examen4, promedioFinal;
+
+    cout <<"Inserte el resultado del primer examen: ";
+    cin >> examen1;
+
+    cout <<"Inserte el resultado del segundo examen: ";
+    cin >> examen2;
+
+    cout <<"Inserte el resultado del tercer examen: ";
+    cin >> examen3;
+
+    cout <<"Inserte el resultado del cuarto examen: ";
+    cin >> examen4;
+    //proceso para obtener promedio final
+    promedioFinal = examen1 + examen2 + examen3 + examen4;
+    //resultado del promedio final
+    cout <<"El promedio final obtenido fue: "<< promedioFinal <<" puntos.";
+
+    return 0;
+}
