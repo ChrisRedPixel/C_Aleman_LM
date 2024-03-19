8b5918042d3ad48c96a371917a5701cd2057f31e
Christian Alemán <chris.aleman2005@gmail.com>
2024-03-18 22:20:52 -0400

    New C++ file

---
+++ Labs C++/Actividad_1/6-AreaPerimetroRectangulo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//proposito: buscar area de un cuadrado
+
+int main()
+{
+    double area, base, altura, perimetro;
+
+    cout <<"Introduzca la longitud de la base del rectangulo: ";
+    cin >> base;
+
+    cout <<"Introduzca la altura del rectangulo: ";
+    cin >> altura;
+
+    area = base * altura;
+
+    cout <<"El area del rectangulo es: "<< area <<endl;
+
+    perimetro = 2 * base + 2 * altura;
+
+    cout <<"El perimetro del rectangulo es: "<< perimetro <<endl;
+
+    return 0;
+
+}
