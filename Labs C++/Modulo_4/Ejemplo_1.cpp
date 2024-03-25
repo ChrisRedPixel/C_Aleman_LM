b2027471412ccb242f98bb54591cff35ace64fc0
Christian Alemán <chris.aleman2005@gmail.com>
2024-03-25 09:55:32 -0400

    New fileSSSSSSS

---
+++ Labs C++/Modulo_4/Ejemplo_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+
+    cout << "Introduce un número: ";
+    cin >> n;
+
+    if (n > 0)
+    {
+        cout << "El número es positivo" << endl;
+    }
+    
+    return 0;
+
+}
---
+++ LabsC#/EjemploMod_4.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EjemploMod_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0) {
+                Console.WriteLine("El número es positivo");
+            }
+        }
+    }
+}
