e53bde9f56171f4ef3373f02f06f4fd7fe836cd0
Christian Alemán <chris.aleman2005@gmail.com>
2024-04-10 21:35:35 -0400

    New C# file

---
+++ LabsC#/Modulo_5/cicloForNombre.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cual es su nombre?: ");//Se inserta el nombre del usador
+            string suNombre = string.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i = i + 1)
+            {
+                Console.WriteLine("Hola " + suNombre);//El nombre que se insertó se imprime
+            }
+        }
+    }
+}
