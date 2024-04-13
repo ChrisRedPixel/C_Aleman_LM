e9292b53c1f7e34a6f183e265c9f9d365882bb0d
Christian Alemán <chris.aleman2005@gmail.com>
2024-04-13 13:49:58 -0400

    Files updated

--- Labs C++/Modulo_5/ciclo_doWhileTemp.cpp
+++ Labs C++/Modulo_5/ciclo_doWhileTemp.cpp
@@ -8,16 +8,22 @@ int main()
 
     do
     {
+        //se inserta la temperatura durante el mes
         cout << ("Inserte la temperatura del mes: ");
         cin >> temperatura;
 
+        //se sigue hacia el siguiente mes
         mes = mes + 1;
+
+        //se suma la temperatura insertada al promedio
         promedio = promedio + temperatura;
     } while (mes < 12);
 
+    //se calcula el promedio
     promedio = promedio / 12;
 
+    //resultado final del promedio
     cout <<"El promedio de las de 12 temperaturas recogidas durante los meses de un año es: "<< promedio <<endl;
-    
+
     return 0;
 }
--- LabsC#/cicloWhile_Temp.cs/Program.cs
+++ LabsC#/cicloWhile_Temp.cs/Program.cs
@@ -10,15 +10,21 @@ namespace MyApp
 
             while(mes < 12)
             {
+                //se inserta la temperatura durante el mes
                 Console.Write("Inserte la temperatura del mes: ");
                 double temperatura = double.Parse(Console.ReadLine());
 
+                //se sigue hacia el siguiente mes
                 mes = mes + 1;
+
+                //se suma la temperatura insertada al promedio
                 promedio = promedio + temperatura;
             }
 
+            //se calcula el promedio
             promedio = promedio / 12;
 
+            //resultado final del promedio
             Console.WriteLine("El promedio de las de 12 temperaturas recogidas durante los meses de un año es: " + promedio);
         }
     }
