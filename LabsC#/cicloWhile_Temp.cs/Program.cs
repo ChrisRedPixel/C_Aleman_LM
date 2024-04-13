e288ff3d15f299c3d5e9ef125325d6cfcd784a6d
Christian Alemán <chris.aleman2005@gmail.com>
2024-04-13 12:31:04 -0400

    New C++ file

--- LabsC#/cicloWhile_Temp.cs/Program.cs
+++ LabsC#/cicloWhile_Temp.cs/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mes = 0, promedio = 0;
+
+            while(mes < 12)
+            {
+                Console.Write("Inserte la temperatura del mes: ");
+                double temperatura = double.Parse(Console.ReadLine());
+
+                mes = mes + 1;
+                promedio = promedio + temperatura;
+            }
+
+            promedio = promedio / 12;
+
+            Console.WriteLine("El promedio de las de 12 temperaturas recogidas durante los meses de un año es: " + promedio);
+        }
+    }
+}
