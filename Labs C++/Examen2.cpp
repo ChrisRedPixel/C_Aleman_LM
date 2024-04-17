3e12ad372234b16e54701acf066ca056d64d66bd
Christian Alemán <chris.aleman2005@gmail.com>
2024-04-17 09:01:42 -0400

    YAY

---
+++ Labs C++/Examen2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero = 0;
+
+    while (numero < 5)
+    {
+        numero = numero + 1;
+        cout << numero <<endl;
+    }
+    
+    return 0;
+}
